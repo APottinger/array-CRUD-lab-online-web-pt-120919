247ca1e459dbf9d0b4db5b39c32b43b98ad32974
Allen Phillip Pottinger <allenpottinger@gmail.com>
2019-12-22 00:59:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,7 +4,6 @@ end
 
 def create_an_array
  basketball_players = ["Kobe Bryant", "Lebron James", "James Harden", "Michael Jordan"]
-end
 
 def add_element_to_end_of_array
   basketball_players << "Ben Simmons"
